2d9f222ba222803757c6dd5caedcea11295a0cf0
Austin Bean <austinbean@gmail.com>
2019-07-19 21:16:48 -0400

    more edits

--- sc_classifier.R
+++ sc_classifier.R
@@ -2,10 +2,14 @@ library(randomForest)
 library(dplyr)
 
 # scp  "/Users/austinbean/Desktop/drgml/sc_classifier.R" beanaus@hsrdcsub2.pmacs.upenn.edu:/project/Lorch_project2018/bean/
+# scp beanaus@hsrdcsub2.pmacs.upenn.edu:/project/Lorch_project2018/bean/var_imp_sc.csv   "/Users/austinbean/Desktop/drgml/" 
+# scp beanaus@hsrdcsub2.pmacs.upenn.edu:/project/Lorch_project2018/bean/sc_test_predict.csv   "/Users/austinbean/Desktop/drgml/" 
 
+# when submitting use bsub < r_sc_class.sh -m pellaeon 
 
 # constant parameter
-tree_num = 3
+tree_num = 100
+set.seed(26)
 
 # forest combining function
 source("/project/Lorch_project2018/bean/forest_combiner.R")
@@ -16,77 +20,83 @@ test_d = read.csv("/project/Lorch_project2018/bean/sc_nicu_test.csv")
 # what should I do... can't sort and separate out.
 # Just take half of each for each thing.
 
-
-# maybe this column is just missing and it should be admn_nicu
-  # select the admitted patients.
-      train_d_ad <- train_d[train_d$admn_nicu == 1, ]
-    # number of rows
-      rows_t1 = dim(train_d_ad)[1]
-      midpoint_t1 = floor(rows_t1/2)
-    # create two halves of admitted patients
-      train_d_ad_1 = train_d_ad[1:midpoint_t1,]
-      train_d_ad_2 = train_d_ad[(midpoint_t1+1):rows_t1,]
+# combine columns
+alld <- bind_rows(train_d, test_d)
+alld <- mutate_if(alld, is.numeric, as.factor)
 
 
-    # select half the unadmitted patients
-      train_d_na <- train_d[train_d$admn_nicu == 0, ]
-    # number of rows
-      rows_t2 = dim(train_d_na)[1]
-      midpoint_t2 = floor(rows_t2/2)
-    # two halves of not admitted patients
-      train_d_na_1 = train_d_na[1:midpoint_t2,]
-      train_d_na_2 = train_d_na[(midpoint_t2+1):rows_t2,]
-      
-# combine them earlier...
-      train_1 <- rbind(train_d_ad_1, train_d_na_1)
-      train_2 <- rbind(train_d_ad_2, train_d_na_2)
+# add column of random values:
+alld$r_v <- runif(nrow(alld))
+alld$set <- rep(0, nrow(alld))
 
-# add column.  Stupid
-      train_1$lab1 <-rep(1, nrow(train_1)) 
-      train_2$lab1 <-rep(2, nrow(train_2)) 
-      test_d$lab1 <-rep(3, nrow(test_d)) 
+# select out 60/30/10 split:
+for (i in 1:nrow(alld)){
+  if (alld[i, "r_v"] <= 0.6) {
+    alld[i,"r_v"] = 1
+  }
+  # check if this is short-circuit and
+  else if ((alld[i, "r_v"] > 0.6)&&(alld[i, "r_v"] <= 0.9)){
+    alld[i,"r_v"] = 2
+  }
+  else{
+    alld[i, "r_v"] = 3
+  }
+  if (i%%10000 == 0) {
+    print(i)
+  }
+}
 
-# r bind the list of all four of them:
-      new_df <- bind_rows(train_1, train_2, test_d)
+# subset out: 
 
-# reselect out:
-      train_1 <- new_df[new_df$lab1 == 1, ]
-      train_2 <- new_df[new_df$lab1 == 2, ]
-      test_d <- new_df[new_df$lab1 == 3, ]
-      
+traind <- alld[alld$r_v == 1,]
+vald <- alld[alld$r_v == 2,]
+testd <- alld[alld$r_v == 3,]
+
 # rename
-colnames(train_1) <- toupper(colnames(train_1))
-colnames(train_2) <- toupper(colnames(train_2))
-colnames(test_d) <- toupper(colnames(test_d))
+colnames(traind) <- toupper(colnames(traind))
+colnames(vald) <- toupper(colnames(vald))
+colnames(testd) <- toupper(colnames(testd))
 
-# convert numeric variables to factors
-train_1 <- mutate_if(train_1, is.numeric, as.factor)
-train_2 <- mutate_if(train_2, is.numeric, as.factor)
-test_d <- mutate_if(test_d, is.numeric, as.factor)
 
 # dump row number and Record ID
-train_1$PID <- NULL
-train_1$RECORD_ID <- NULL
-train_1$LAB1 <- NULL
+traind$PID <- NULL
+traind$RECORD_ID <- NULL
+traind$LAB1 <- NULL
+traind$R_V <- NULL
+traind$SET <- NULL
 
-train_2$PID <- NULL
-train_2$RECORD_ID <- NULL
-train_2$LAB1 <- NULL
+vald$PID <- NULL
+vald$RECORD_ID <- NULL
+vald$LAB1 <- NULL
+vald$R_V <- NULL
+vald$SET <- NULL
 
-test_d$PID <- NULL
-test_d$RECORD_ID <- NULL
-test_d$LAB1 <- NULL
+testd$PID <- NULL
+testd$RECORD_ID <- NULL
+testd$LAB1 <- NULL
+testd$R_V <- NULL
+testd$SET <- NULL
+
+# split data into two
+
+dim1 = nrow(traind)
+train_1 <- traind[1:floor(dim1/2),]
+train_2 <- traind[(floor(dim1/2)+1):dim1,]
 
 forest_tst1 <- randomForest(ADMN_NICU~ .,
                             data=train_1,
                             ntree = tree_num,
                             do.trace=TRUE,
+                            proximity=FALSE,
+                            nodesize=100,
                             importance=TRUE)
 
 forest_tst2 <- randomForest(ADMN_NICU~ .,
                             data=train_2,
                             ntree = tree_num,
                             do.trace=TRUE,
+                            proximity=FALSE,
+                            nodesize=100,
                             importance=TRUE)
 
 
@@ -96,10 +106,15 @@ library(randomForest)
 forest_tst <- forest_combine(forest_tst1, forest_tst2)
 
 res <-predict(forest_tst, 
-              test_d,
+              testd,
               type="response")
 
-sum(res!=test_d$ADMN_NICU)/length(res)
+sum(res!=testd$ADMN_NICU)/length(res)
+
+
+testd$pred_results <- res
+#test_d$num_pred <- as.numeric(levels(res))[res]
+write.csv(testd, file="/project/Lorch_project2018/bean/sc_test_predict.csv")
 
 
 imp_vals <- importance(forest_tst)
