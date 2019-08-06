2bc2f95c4ab3dbec095fd01d78514b8575a56268
Austin B. Bean <austinbean@gmail.com>
2019-08-06 14:15:41 -0400

    tablereader

--- nicu_classifier_v2.jl
+++ nicu_classifier_v2.jl
@@ -3,7 +3,7 @@
 # use the nicu_collapsed matrix
 # use each column as a predictor
 # trees should be trained on something like 50 predictors
-# and a few hundred trees.  
+# and a few hundred trees.
 
 #=
 julia nicu_classifier_v2.jl
@@ -17,27 +17,45 @@ Classify
 Correct : 0.998689177852349
 =#
 
+#=
+TODO - there is a way to do two things:
+- parallelize the training process.  That isn't working right now.
+- use multithreading on the classification.  That sound be easy.
+
+
+
+=#
+
 # Transfer the result:
 # scp  "/Users/austinbean/Desktop/drgml/model_nicu.jld2" beanaus@hsrdcsub2.pmacs.upenn.edu:/project/Lorch_project2018/bean/
- 
 
 
-using DataFrames, StatFiles, DecisionTree, ScikitLearn, Query, Dates, JLD2, CSVFiles, Serialization
+using DataFrames, StatFiles, DecisionTree, ScikitLearn, Query, Dates, JLD2, TableReader, Serialization, Distributed
 
+#@everywhere using DataFrames, StatFiles, DecisionTree, ScikitLearn, Query, Dates, JLD2, TableReader, Serialization, Distributed
+
+# maybe tablereader is better than csvfiles ????
+# TODO - switch to tablereader.  Also cut down the purity of the tree, or min node size.  It's generating too many leaves now.
 
 
 function DoIt(x::Int64)
 	println(VERSION)
+	#addprocs()
+
+	path = "tuk39938"
+	#path = "austinbean"
 
 	println("opening data ", Dates.format(now(), "d/m/Y HH:MM"))
 
-	df_test = DataFrame(load("/Users/austinbean/Desktop/drgml/quick_test.csv"))
+	df_test = TableReader.readcsv("/Users/$path/Desktop/programs/drgml/tx_d1.csv")
 
 	println("data opened ", Dates.format(now(), "d/m/Y HH:MM"))
 		train_dat = df_test;
+			# TODO - change next line for new indexing scheme
 		tf = train_dat[names(train_dat)[2:end-1]]
 		tf = convert( Array{Union{Missing, Int64}, 2}, tf)
-		tlabels = train_dat[:ADMN_NICU]
+			# there is a dep warn about the indexing scheme
+		tlabels = train_dat[:admn_nicu]
 
 
 
@@ -52,40 +70,42 @@ function DoIt(x::Int64)
 	                          min_samples_split,
 	                          min_purity_increase)
 				=#
-
-	modeln = build_forest(tlabels, tf, -1, 1, 0.7, -1, 5, 2, 0.0)
+					# TODO - lower sample, probably.  Lower the last four so this is faster.
+					# Makes sense to subsample fewer points, since taking an ensemble at the end anyway.
+					# cutting "partial_sampling" down to 0.1 speeds up by a factor of 3
+	modeln = build_forest(tlabels, tf, -1, 250, 0.2, -1, 5, 2, 0.0)
 	println("forest trained ", Dates.format(now(), "d/m/Y HH:MM"))
 
 	println("Loading holdout  ", Dates.format(now(), "d/m/Y HH:MM"))
-	df_t = DataFrame(load("/Users/austinbean/Desktop/drgml/quick_val.csv"))
+		# this is going to run on the TX 2000 data.
+	df_t = TableReader.readcsv("/Users/$path/Desktop/programs/drgml/tx00.csv")
 
 	println("Testing Model ", Dates.format(now(), "d/m/Y HH:MM"))
 	test_set = df_t
-		tf = test_set[names(test_set)[2:end-1]]
+		tf = test_set[names(test_set)[3:end-1]]
 		tf = convert( Array{Union{Missing, Int64}, 2}, tf)
-		tlabels = test_set[:ADMN_NICU]
+		#tlabels = test_set[:admn_nicu]
 	println("Classify ", Dates.format(now(), "d/m/Y HH:MM"))
-		output = zeros(size(tlabels,1) , 2)
-		for i = 1:size(tlabels,1)
-			output[i, 1] = tlabels[i]
+		output = zeros(size(tf,1) , 2)
+		for i = 1:size(tf,1)
+			#output[i, 1] = tlabels[i]
 			output[i, 2] = convert(Float64, apply_forest(modeln, tf[i, :]))
-		end 
-		correct_f = 1- sum(abs.(output[:,1] .- output[:,2]))/size(output,1)
-	@save "/Users/austinbean/Desktop/drgml/model_nicu.jld2" modeln
+		end
+		#correct_f = 1- sum(abs.(output[:,1] .- output[:,2]))/size(output,1)
+	correct_f = sum(output, dims=1)[2]
+	#@save "/Users/$path/Desktop/programs/drgml/model_nicu.jld2" modeln
 
-	println("Correct : ", correct_f, "   ", Dates.format(now(), "d/m/Y HH:MM"))
+	println("Adm predict : ", correct_f, "   ", Dates.format(now(), "d/m/Y HH:MM"))
 	# New Test to save using serializer.
 
 
-	# can serialize the object if necessary, though this requires JL>1.1  
-	open("/Users/austinbean/Desktop/drgml/saved_thing.jls", "w") do file 
+	# can serialize the object if necessary, though this requires JL>1.1
+	open("/Users/$path/Desktop/programs/drgml/saved_thing.jls", "w") do file
 		serialize(file, modeln)
 		close(file)
-	end 
+	end
 
 
-end 
+end
 
 DoIt(0)
-
-
